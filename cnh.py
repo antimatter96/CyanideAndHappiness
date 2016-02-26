78e8329a6d37b6c1f92047a309df9b3b04ea411b
Arpit Jain <antimatter96@users.noreply.github.com>
2016-02-26 20:38:45 +0530

    Update cnh.py

--- cnh.py
+++ cnh.py
@@ -2,14 +2,14 @@ import requests
 from bs4 import BeautifulSoup
 
 
-i=15														#Starts from 15 , earlier pages do not exist
+i=15									       #Starts from 15 , earlier pages do not exist
 while i <= 4225:
 	url_cnh = "http://explosm.net/comics/" + str(i)
 	r1 = requests.get(url_cnh)
 	if r1.status_code == 200:
 		soup = BeautifulSoup(r1.text,"html.parser")
 		file_link = soup.find(id="main-comic")['src']
-		if file_link[0]!= "h":								#Some links have http: while some dont				
+		if file_link[0]!= "h":						#Some links have http: while some dont				
 			file_link = "http:" + file_link
 		file_name = 	file_link.split('/')[-1]
 		r2 = requests.get(file_link)
